0d9374f87eb872ec254bd3e7890239f1c6c664a6
George Small <geomall92@gmail.com>
2025-07-28 05:05:13 -0400

    Changed over to PJRC Teensy SD and SDFat

--- examples/CardInfo/CardInfo.ino
+++ examples/CardInfo/CardInfo.ino
@@ -1,25 +1,3 @@
-/*
-  SD card test
-
-  This example shows how use the utility libraries on which the
-  SD library is based in order to get info about your SD card.
-  Very useful for testing a card when you're not sure whether its working or not.
-  Pin numbers reflect the default SPI pins for Uno and Nano models.
-  The circuit:
-    SD card attached to SPI bus as follows:
- ** SDO - pin 11 on Arduino Uno/Duemilanove/Diecimila
- ** SDI - pin 12 on Arduino Uno/Duemilanove/Diecimila
- ** CLK - pin 13 on Arduino Uno/Duemilanove/Diecimila
- ** CS - depends on your SD card shield or module.
- 		Pin 10 used here for consistency with other Arduino examples
-
-  created  28 Mar 2011
-  by Limor Fried
-  modified 24 July 2020
-  by Tom Igoe
-*/
-// include the SD library:
-#include <SPI.h>
 #include <SD.h>
 
 // set up variables using the SD utility library functions:
--- src/SD.h
+++ src/SD.h
@@ -1,138 +1,286 @@
-/*
-
-  SD - a slightly more friendly wrapper for sdfatlib
-
-  This library aims to expose a subset of SD card functionality
-  in the form of a higher level "wrapper" object.
-
-  License: GNU General Public License V3
-          (Because sdfatlib is licensed with this.)
-
-  (C) Copyright 2010 SparkFun Electronics
-
-*/
+/* SD library compatibility wrapper for use of SdFat on Teensy
+ * Copyright (c) 2020, Paul Stoffregen, paul@pjrc.com
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice, development funding notice, and this permission
+ * notice shall be included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
 
 #ifndef __SD_H__
 #define __SD_H__
 
 #include <Arduino.h>
+#include <SdFat.h>
+#if !defined(SD_FAT_TEENSY_MODIFIED)
+#error "Teensy's SD library uses a custom modified copy of SdFat.  Standard SdFat was mistakenly used.  Arduino should print multiple libraries found for SdFat.h.  To resolve this error, you will need to move or delete the copy Arduino is using, or otherwise take steps to cause Teensy's special copy of SdFat to be used."
+#endif
+// Use FILE_READ & FILE_WRITE as defined by FS.h
+#if defined(FILE_READ) && !defined(FS_H)
+#undef FILE_READ
+#endif
+#if defined(FILE_WRITE) && !defined(FS_H)
+#undef FILE_WRITE
+#endif
+#include <FS.h>
 
-#include "utility/SdFat.h"
-#include "utility/SdFatUtil.h"
+class SD_clock_class
+{
+public:
+    static unsigned long get(void) __attribute__((always_inline)) { return HAL_GetTick() / 1000; }
+};
+extern SD_clock_class SDClock;
 
-#define FILE_READ O_READ
-#define FILE_WRITE (O_READ | O_WRITE | O_CREAT | O_APPEND)
 
-namespace SDLib {
+// #if defined(__MK64FX512__) || defined(__MK66FX1M0__) || defined(__IMXRT1062__)
+// #define BUILTIN_SDCARD 254
+// #endif
 
-  class File : public Stream {
-    private:
-      char _name[13]; // our name
-      SdFile *_file;  // underlying file pointer
+// #if defined(__arm__)
+  // Support everything on 32 bit boards with enough memory
+  #define SDFAT_FILE FsFile
+  #define SDFAT_BASE SdFs
+  #define MAX_FILENAME_LEN 256
+// #elif defined(__AVR__)
+  // Limit to 32GB cards on 8 bit Teensy with only limited memory
+//   #define SDFAT_FILE File32
+//   #define SDFAT_BASE SdFat32
+//   #define MAX_FILENAME_LEN 64
+// #endif
 
-    public:
-      File(SdFile f, const char *name);     // wraps an underlying SdFile
-      File(void);      // 'empty' constructor
-      virtual size_t write(uint8_t);
-      virtual size_t write(const uint8_t *buf, size_t size);
-      virtual int availableForWrite();
-      virtual int read();
-      virtual int peek();
-      virtual int available();
-      virtual void flush();
-      int read(void *buf, uint16_t nbyte);
-      bool seek(uint32_t pos);
-      uint32_t position();
-      uint32_t size();
-      void close();
-      operator bool();
-      char * name();
-
-      bool isDirectory(void);
-      File openNextFile(uint8_t mode = O_RDONLY);
-      void rewindDirectory(void);
-
-      using Print::write;
-  };
-
-  class SDClass {
-
-    private:
-      // These are required for initialisation and use of sdfatlib
-      Sd2Card card;
-      SdVolume volume;
-      SdFile root;
-
-      // my quick&dirty iterator, should be replaced
-      SdFile getParentDir(const char *filepath, int *indx);
-    public:
-      // This needs to be called to set up the connection to the SD card
-      // before other methods are used.
-      bool begin(uint8_t csPin = SD_CHIP_SELECT_PIN);
-      bool begin(uint32_t clock, uint8_t csPin);
-
-      //call this when a card is removed. It will allow you to insert and initialise a new card.
-      void end();
-
-      // Open the specified file/directory with the supplied mode (e.g. read or
-      // write, etc). Returns a File object for interacting with the file.
-      // Note that currently only one file can be open at a time.
-      File open(const char *filename, uint8_t mode = FILE_READ);
-      File open(const String &filename, uint8_t mode = FILE_READ) {
-        return open(filename.c_str(), mode);
-      }
-
-      // Methods to determine if the requested file path exists.
-      bool exists(const char *filepath);
-      bool exists(const String &filepath) {
-        return exists(filepath.c_str());
-      }
-
-      // Create the requested directory heirarchy--if intermediate directories
-      // do not exist they will be created.
-      bool mkdir(const char *filepath);
-      bool mkdir(const String &filepath) {
-        return mkdir(filepath.c_str());
-      }
-
-      // Delete the file.
-      bool remove(const char *filepath);
-      bool remove(const String &filepath) {
-        return remove(filepath.c_str());
-      }
-
-      bool rmdir(const char *filepath);
-      bool rmdir(const String &filepath) {
-        return rmdir(filepath.c_str());
-      }
-
-    private:
-
-      // This is used to determine the mode used to open a file
-      // it's here because it's the easiest place to pass the
-      // information through the directory walking function. But
-      // it's probably not the best place for it.
-      // It shouldn't be set directly--it is set via the parameters to `open`.
-      int fileOpenMode;
-
-      friend class File;
-      friend bool callback_openPath(SdFile&, const char *, bool, void *);
-  };
-
-  extern SDClass SD;
-
+class SDFile : public FileImpl
+{
+private:
+    // Classes derived from File are never meant to be constructed
+    // anywhere other than open() in the parent FS class and
+    // openNextFile() while traversing a directory.
+    // Only the abstract File class which references these derived
+    // classes is meant to have a public constructor!
+    SDFile(const SDFAT_FILE &file) : sdfatfile(file), filename(nullptr) { }
+    friend class SDClass;
+public:
+    virtual ~SDFile(void) {
+        close();
+    }
+    virtual size_t write(const void *buf, size_t size) {
+        return sdfatfile.write(buf, size);
+    }
+    virtual int peek() {
+        return sdfatfile.peek();
+    }
+    virtual int available() {
+        return sdfatfile.available();
+    }
+    virtual void flush() {
+        sdfatfile.flush();
+    }
+    virtual size_t read(void *buf, size_t nbyte) {
+        return sdfatfile.read(buf, nbyte);
+    }
+    virtual bool truncate(uint64_t size=0) {
+        return sdfatfile.truncate(size);
+    }
+    virtual bool seek(uint64_t pos, int mode = SeekSet) {
+        if (mode == SeekSet) return sdfatfile.seekSet(pos);
+        if (mode == SeekCur) return sdfatfile.seekCur(pos);
+        if (mode == SeekEnd) return sdfatfile.seekEnd(pos);
+        return false;
+    }
+    virtual uint64_t position() {
+        return sdfatfile.curPosition();
+    }
+    virtual uint64_t size() {
+        return sdfatfile.size();
+    }
+    virtual void close() {
+        if (filename) {
+            free(filename);
+            filename = nullptr;
+        }
+        if (sdfatfile.isOpen()) {
+            sdfatfile.close();
+        }
+    }
+    virtual bool isOpen() {
+        return sdfatfile.isOpen();
+    }
+    virtual const char * name() {
+        if (!filename) {
+            filename = (char *)malloc(MAX_FILENAME_LEN);
+            if (filename) {
+                sdfatfile.getName(filename, MAX_FILENAME_LEN);
+            } else {
+                static char zeroterm = 0;
+                filename = &zeroterm;
+            }
+        }
+        return filename;
+    }
+    virtual boolean isDirectory(void) {
+        return sdfatfile.isDirectory();
+    }
+    virtual File openNextFile(uint8_t mode=0) {
+        SDFAT_FILE file = sdfatfile.openNextFile();
+        if (file) return File(new SDFile(file));
+        return File();
+    }
+    virtual void rewindDirectory(void) {
+        sdfatfile.rewindDirectory();
+    }
+    virtual bool getCreateTime(DateTimeFields &tm) {
+        uint16_t fat_date, fat_time;
+        if (!sdfatfile.getCreateDateTime(&fat_date, &fat_time)) return false;
+        if ((fat_date == 0) && (fat_time == 0)) return false;
+        tm.sec = FS_SECOND(fat_time);
+        tm.min = FS_MINUTE(fat_time);
+        tm.hour = FS_HOUR(fat_time);
+        tm.mday = FS_DAY(fat_date);
+        tm.mon = FS_MONTH(fat_date) - 1;
+        tm.year = FS_YEAR(fat_date) - 1900;
+        return true;
+    }
+    virtual bool getModifyTime(DateTimeFields &tm) {
+        uint16_t fat_date, fat_time;
+        if (!sdfatfile.getModifyDateTime(&fat_date, &fat_time)) return false;
+        if ((fat_date == 0) && (fat_time == 0)) return false;
+        tm.sec = FS_SECOND(fat_time);
+        tm.min = FS_MINUTE(fat_time);
+        tm.hour = FS_HOUR(fat_time);
+        tm.mday = FS_DAY(fat_date);
+        tm.mon = FS_MONTH(fat_date) - 1;
+        tm.year = FS_YEAR(fat_date) - 1900;
+        return true;
+    }
+    virtual bool setCreateTime(const DateTimeFields &tm) {
+        if (tm.year < 80 || tm.year > 207) return false;
+        return sdfatfile.timestamp(T_CREATE, tm.year + 1900, tm.mon + 1,
+            tm.mday, tm.hour, tm.min, tm.sec);
+    }
+    virtual bool setModifyTime(const DateTimeFields &tm) {
+        if (tm.year < 80 || tm.year > 207) return false;
+        return sdfatfile.timestamp(T_WRITE, tm.year + 1900, tm.mon + 1,
+            tm.mday, tm.hour, tm.min, tm.sec);
+    }
+private:
+    SDFAT_FILE sdfatfile;
+    char *filename;
 };
 
-// We enclose File and SD classes in namespace SDLib to avoid conflicts
-// with others legacy libraries that redefines File class.
 
-// This ensure compatibility with sketches that uses only SD library
-using namespace SDLib;
 
-// This allows sketches to use SDLib::File with other libraries (in the
-// sketch you must use SDFile instead of File to disambiguate)
-typedef SDLib::File    SDFile;
-typedef SDLib::SDClass SDFileSystemClass;
-#define SDFileSystem   SDLib::SD
+class SDClass : public FS
+{
+public:
+    SDClass() { }
+    bool begin(uint8_t csPin = 10);
+    File open(const char *filepath, uint8_t mode = FILE_READ) {
+        oflag_t flags = O_READ;
+        if (mode == FILE_WRITE) flags = O_RDWR | O_CREAT | O_AT_END;
+        else if (mode == FILE_WRITE_BEGIN) flags = O_RDWR | O_CREAT;
+        SDFAT_FILE file = sdfs.open(filepath, flags);
+        if (file) return File(new SDFile(file));
+        return File();
+    }
+    bool exists(const char *filepath) {
+        return sdfs.exists(filepath);
+    }
+    bool mkdir(const char *filepath) {
+        return sdfs.mkdir(filepath);
+    }
+    bool rename(const char *oldfilepath, const char *newfilepath) {
+        return sdfs.rename(oldfilepath, newfilepath);
+    }
+    bool remove(const char *filepath) {
+        return sdfs.remove(filepath);
+    }
+    bool rmdir(const char *filepath) {
+        return sdfs.rmdir(filepath);
+    }
+    uint64_t usedSize() {
+        if (!cardPreviouslyPresent) return (uint64_t)0;
+        return (uint64_t)(sdfs.clusterCount() - sdfs.freeClusterCount())
+          * (uint64_t)sdfs.bytesPerCluster();
+    }
+    uint64_t totalSize() {
+        if (!cardPreviouslyPresent) return (uint64_t)0;
+        return (uint64_t)sdfs.clusterCount() * (uint64_t)sdfs.bytesPerCluster();
+    }
+    bool format(int type=0, char progressChar=0, Print& pr=Serial);
+    bool mediaPresent();
 
+    // call to allow you to specify if your SD reader has an IO pin that
+    // can be used to detect if an chip is inserted.  On BUILTIN_SDCARD
+    // we will default to use it if you use the begin method, howver
+    // if you bypass this and call directly to SDFat begin, then you
+    // can use this to let us know. 
+    bool setMediaDetectPin(uint8_t pin);
+
+public: // allow access, so users can mix SD & SdFat APIs
+    SDFAT_BASE sdfs;
+    operator SDFAT_BASE & () { return sdfs; }
+    static void dateTime(uint16_t *date, uint16_t *time);
+private:
+    bool cardPreviouslyPresent = false;
+    uint8_t csPin_ = 0xff;
+    uint8_t cdPin_ = 0xff;
+};
+
+extern SDClass SD;
+
+// do not expose these defines in Arduino sketches or other libraries
+#undef SDFAT_FILE
+#undef SDFAT_BASE
+#undef MAX_FILENAME_LEN
+
+
+#define SD_CARD_TYPE_SD1 0
+#define SD_CARD_TYPE_SD2 1
+#define SD_CARD_TYPE_SDHC 3
+class Sd2Card
+{
+public:
+    bool init(uint32_t speed, uint8_t csPin) {
+        return SD.begin(csPin);
+    }
+    uint8_t type() {
+        return SD.sdfs.card()->type();
+    }
+};
+class SdVolume
+{
+public:
+    bool init(Sd2Card &card) {
+        return SD.sdfs.vol() != nullptr;
+    }
+    uint8_t fatType() {
+        return SD.sdfs.vol()->fatType();
+    }
+    uint32_t blocksPerCluster() {
+        return SD.sdfs.vol()->sectorsPerCluster();
+    }
+    uint32_t clusterCount() {
+        return SD.sdfs.vol()->clusterCount();
+    }
+#if defined(__arm__)
+    operator FsVolume * () __attribute__ ((deprecated("Use SD.begin() to access SD cards"))) {
+#elif defined(__AVR__)
+    operator FatVolume * () __attribute__ ((deprecated("Use SD.begin() to access SD cards"))) {
 #endif
+        return SD.sdfs.vol();
+    }
+};
+
+#endif /* __SD_H__  */
